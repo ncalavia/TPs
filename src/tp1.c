a922b4aeb6ba001e3bbf64d7fffdfc3502ccd493
Nico C <nicocalavia@gmail.com>
2019-08-31 18:01:57 -0300

    validacion de numeros en el ingreso

--- src/tp1.c
+++ src/tp1.c
@@ -8,34 +8,40 @@
  ============================================================================
  */
 
-#include <stdio.h>
+#include <stdio_ext.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define RETORNO_EXITOSO 0
 #define RETORNO_ INVALIDO -1
 
-int ingresoNumeros(float *num1,float *num2);
+int ingresoNumeros(int *num1,int *num2);
+int esNumero(int prenumero);
 
-int main(void) {
-
-	float num1;
-	float num2;
+int main(void)
+{
+	int num1;
+	int num2;
 
 	ingresoNumeros(&num1, &num2);
-	return EXIT_SUCCESS;
-}
-
-
-
-int ingresoNumeros(float *numero1, float *numero2)
-{
-	float prenumero1;
-	float prenumero2;
-
-	printf("Ingrese el 1er numero");
-	scanf("%f", &prenumero1);
-	printf("Ingrese el 2do numero");
-	scanf("%f", &prenumero2);
-	printf("primernumero %f",prenumero1);
-	printf("%f",prenumero2);
 	return 0;
 }
+
+int ingresoNumeros(int *numero1, int *numero2)
+{
+	int prenumero1;
+	int prenumero2;
+
+	do
+	{
+		printf("Ingrese el 1er numero");
+		__fpurge(stdin);
+	} while (scanf("%d", &prenumero1) != 1);
+
+	do
+	{
+		printf("Ingrese el 2do numero");
+		__fpurge(stdin);
+	} while (scanf("%d", &prenumero2) != 1);
+	return 0;
+}
+
