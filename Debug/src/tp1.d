5c086f97c7519e748b8bac4bdf192b6c2c314848
Nico C <nicocalavia@gmail.com>
2019-08-31 16:36:33 -0300

    Ingreso numeros

---
+++ Debug/src/tp1.d
@@ -0,0 +1,1 @@
+src/tp1.o: ../src/tp1.c
--- src/tp1.c
+++ src/tp1.c
@@ -10,8 +10,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define RETORNO_EXITOSO 0
+#define RETORNO_ INVALIDO -1
+
+int ingresoNumeros(float *num1,float *num2);
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	float num1;
+	float num2;
+
+	ingresoNumeros(&num1, &num2);
 	return EXIT_SUCCESS;
+}
+
+
+
+int ingresoNumeros(float *numero1, float *numero2)
+{
+	float prenumero1;
+	float prenumero2;
+
+	printf("Ingrese el 1er numero");
+	scanf("%f", &prenumero1);
+	printf("Ingrese el 2do numero");
+	scanf("%f", &prenumero2);
+	printf("primernumero %f",prenumero1);
+	printf("%f",prenumero2);
+	return 0;
 }
