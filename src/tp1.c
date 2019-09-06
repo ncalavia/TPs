3b8cca0d228a6fb6cc1554352f6703856cc84231
alumno <alumno@lab6pc00>
2019-09-05 22:20:10 -0300

    mostrar ingresos

--- src/tp1.c
+++ src/tp1.c
@@ -17,34 +17,69 @@
 
 
 int pedirOpcionMenu(int *opcionSeleccionada);
-void mostrarMenu();
+int mostrarMenu(float *operando1,int *flagOperando1,float *operando2,int *flagOperando2);
 int opcionMenuValido(int preOpcion);
 int pedirOperando1(float *operando1,int *flagOperando1);
 int pedirOperando2(float *operando2,int *flagOperando2);
-int ejecutarOpcion(int opcionSeleccionada);
+int ejecutarOpcion(int opcionSeleccionada,float *operando1,int *flagOperando1,float *operando2,int *flagOperando2);
+
+
+
 
 int main(void)
 {
 	int opcionSeleccionada;
-
+	float operando1;
+	int flagOperando1=0;
+	float operando2;
+	int flagOperando2=0;
 
 	do
 	{
-		mostrarMenu();
+		mostrarMenu(&operando1,&flagOperando1,&operando2,&flagOperando2);
 		pedirOpcionMenu( &opcionSeleccionada);
-		ejecutarOpcion(opcionSeleccionada);
+		ejecutarOpcion(opcionSeleccionada, &operando1, &flagOperando1, &operando2, &flagOperando2);
 	}while(opcionSeleccionada !=5);
 
 	return 0;
 }
 
-void mostrarMenu()
+int mostrarMenu(float *operando1,int *flagOperando1,float *operando2,int *flagOperando2)
 {
-	printf("1. Ingresar 1er operando (A=x) \n");
-	printf("2. Ingresar 2do operando (B=y)\n");
-	printf("3. Calcular todas las operaciones \n");
-	printf("4. Informar resultados \n");
-	printf("5. Salir \n");
+	if(*flagOperando1 != 1 && *flagOperando2 != 1)
+	{
+		printf("1. Ingresar 1er operando (A=x) \n");
+		printf("2. Ingresar 2do operando (B=y)\n");
+		printf("3. Calcular todas las operaciones \n");
+		printf("4. Informar resultados \n");
+		printf("5. Salir \n");
+	}
+	if(*flagOperando1 == 1 && *flagOperando2 != 1)
+	{
+		printf("1. Ingresar 1er operando (%f) \n",*operando1);
+		printf("2. Ingresar 2do operando (B=y)\n");
+		printf("3. Calcular todas las operaciones \n");
+		printf("4. Informar resultados \n");
+		printf("5. Salir \n");
+	}
+	if(*flagOperando1 != 1 && *flagOperando2 == 1)
+	{
+		printf("1. Ingresar 1er operando (A=x) \n");
+		printf("2. Ingresar 2do operando (%f)\n",*operando2);
+		printf("3. Calcular todas las operaciones \n");
+		printf("4. Informar resultados \n");
+		printf("5. Salir \n");
+	}
+	if(*flagOperando1 == 1 && *flagOperando2 == 1)
+	{
+		printf("1. Ingresar 1er operando (%f)\n",*operando1);
+		printf("2. Ingresar 2do operando (%f)\n",*operando2);
+		printf("3. Calcular todas las operaciones \n");
+		printf("4. Informar resultados \n");
+		printf("5. Salir \n");
+	}
+
+	return 0;
 }
 int pedirOpcionMenu(int *opcionSeleccionada)
 {
@@ -67,15 +102,17 @@ int opcionMenuValido(int preOpcion)
 	}
 	return retorno;
 }
-int pedirOperando1(float *operando1,int *flagOperando1)    // pedir - validar - cambiar valor de bandera al ingresar en operando1
+int pedirOperando1(float *operando1,int *flagOperando1)
 {
-	float preOperando1;
-	int preflagOperando1;
+	float preOperando1=0;
+	int preflagOperando1 = *flagOperando1;
+	int resultadoScanf;
 	do
 	{
 		printf("Ingrese el 1er operando");
-		__fpurge(stdin);
-	} while (scanf("%f", &preOperando1) != 1);
+		//__fpurge(stdin);
+		resultadoScanf=scanf("%f", &preOperando1);
+	} while(resultadoScanf != 1);
 	*operando1 = preOperando1;
 	preflagOperando1 = 1;
 	*flagOperando1 = preflagOperando1;
@@ -83,43 +120,51 @@ int pedirOperando1(float *operando1,int *flagOperando1)    // pedir - validar - 
 }
 int pedirOperando2(float *operando2,int *flagOperando2)    // pedir - validar - cambiar valor de bandera al ingresar en operando1
 {
-	float preOperando2;
-	int preflagOperando2;
+	float preOperando2=0;
+	int preflagOperando2 = *flagOperando2;
+	int resultadoScanf;
+
 	do
 	{
-		printf("Ingrese el 2do operando");
-		__fpurge(stdin);
-	} while (scanf("%f", &preOperando2) != 1);
+	printf("Ingrese el 2do operando");
+	//__fpurge(stdin);
+	resultadoScanf=scanf("%f", &preOperando2);
+	} while(resultadoScanf != 1);
 	*operando2 = preOperando2;
-	preflagOperando2 = 2;
+	preflagOperando2 = 1;
 	*flagOperando2 = preflagOperando2;
 	return 0;
 }
-
-int ejecutarOpcion(int opcionSeleccionada)
+int ejecutarOpcion(int opcionSeleccionada,float *operando1,int *flagOperando1,float *operando2,int *flagOperando2)
 {
-	float operando1;
-	int flagOperando1=0;
-	float operando2;
-	int flagOperando2=0;
 
 	switch(opcionSeleccionada)
 			{
 				case 1:
-					pedirOperando1(&operando1, &flagOperando1);
+					pedirOperando1(operando1, flagOperando1);
 					break;
 				case 2:
-					pedirOperando2(&operando2, &flagOperando2);
+					pedirOperando2(operando2, flagOperando2);
 					break;
 				case 3:
-					if(flagOperando1 == 1 && flagOperando2 == 1)
+					if(*flagOperando1 == 1 && *flagOperando2 == 1)
 					{
-						printf("los dos operando ingresados \n");
+						printf("Calcular todas las operaciones\n");
+						printf("a) Calcular la suma (%f+%f)\n",*operando1,*operando2);
+						printf("b) Calcular la resta (%f-%f)\n",*operando1,*operando2);
+						printf("c) Calcular la division (%f/%f)\n",*operando1,*operando2);
+						printf("d) Calcular la multiplicacion (%f*%f)\n",*operando1,*operando2);
+						printf("e) Calcular el factorial (%f!) \n",*operando1);
+
 					}
 					else
 						printf("Debera ingresar los operando para esta opcion \n");
 					break;
+				case 4:
+					printf("resultado");
+					break;
 			}
+
 	return 0;
 }
 
